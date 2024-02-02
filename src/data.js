ffc3cb7cc6e450d955fc5780fd2832b2f360237e
Lida Susan Sabu <lidasusansabu13.1@gmail.com>
2024-02-02 01:46:00 -0500

    initial portfolio structure

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: "EasyMail",
+    subtitle: "Python and Vue.js",
+    description:
+      "This AI productivity tool scans, sorts, summarizes, and categorizes your emails into personalized categories, such as work, school, finance, etc. Additionally, the platform includes a to-do tab that displays your daily tasks, their summaries, and current status.",
+    image: "./easymail.jpeg",
+    link: "https://devpost.com/software/easymail-i1nrpz",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Program to track hand movements using mediapipe and open CV.",
+    image: "./project4.jpeg",
+    link: "https://github.com/lidasusansabu13/HandTracker",
+  },
+  
+];
+
+export const BlogsLists = [
+  {
+    quote:
+      "Coding best practices are essential guidelines that developers follow to write high-quality, efficient, and maintainable code. One fundamental principle is clarity, where code should be easily understandable to facilitate collaboration and future modifications. Consistent indentation, meaningful variable and function names, and clear comments contribute to code readability. Another crucial aspect is modularization, breaking down complex tasks into smaller, manageable modules or functions. This promotes code reusability and makes debugging and testing more straightforward. Following the DRY (Don't Repeat Yourself) principle helps eliminate redundancy, ensuring that changes only need to be made in one place. Regular version control, through tools like Git, ensures a history of code changes and simplifies collaboration among team members. Additionally, adhering to coding standards, such as those defined by the programming language or the organization, fosters a unified and coherent codebase. Regular code reviews, automated testing, and continuous integration are integral parts of the development process to catch errors early and maintain code quality over time. Embracing these best practices not only enhances code quality but also contributes to a more efficient and enjoyable development experience.",
+    image: "https://avatars.githubusercontent.com/u/25691275?v=4",
+    name: "Coding best practices",
+    company: "Python",
+  },
+  {
+    quote:
+      "Unit testing plays a pivotal role in ensuring the robustness and reliability of software, and PyTest emerges as a powerful tool for conducting these tests in the Python ecosystem. The significance of unit testing using PyTest lies in its ability to systematically evaluate individual units of code, typically functions or methods, in isolation. By verifying that each unit performs as intended, developers can identify and rectify bugs or unexpected behaviors early in the development cycle. PyTest's simplicity and readability make it an excellent choice for writing concise and expressive test cases, promoting a faster and more efficient testing process. Additionally, unit tests act as documentation, providing insights into the expected behavior of specific code components. This documentation aspect becomes invaluable during code maintenance and collaboration, as it helps prevent unintentional changes that could introduce errors. Embracing unit testing with PyTest not only enhances code quality but also fosters a culture of confidence among developers, enabling them to make changes with the assurance that existing functionality remains intact. Ultimately, this proactive approach to testing contributes to the overall stability and maintainability of the software.",
+    image: "https://avatars.githubusercontent.com/u/25691275?v=4",
+    name: "Importance of unit testing",
+    company: "pyTest",
+  },
+];
+
+export const skills = [
+  "Python",
+  "React.js",
+  "PostgreSQL",
+  "AWS",
+  "Git",
+  "SQL Alchemy",
+];
